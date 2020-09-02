fbf2780af80188a5a88ac63d0b0c181b89762c69
AhmedIbrahimHamed <aih1175@gmail.com>
2020-09-02 08:16:19 +0200

    Added Client side SPA

---
+++ DatingSiteSPA/src/app/value/value.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  values: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('https://localhost:44386/api/test').subscribe(resource => {
+      this.values = resource;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
