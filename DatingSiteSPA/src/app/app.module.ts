443e674a5aa8e2de6940f2cb960ed1a5d83e8df4
AhmedIbrahimHamed <aih1175@gmail.com>
2020-09-03 13:02:10 +0200

    Added ng-bootstrap and bootswatch for client side and fixed dropdown menu

--- DatingSiteSPA/src/app/app.module.ts
+++ DatingSiteSPA/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,10 +22,15 @@ import { AlertifyService } from './_services/Alertify.service';
     HomeComponent,
     RegisterComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CollapseModule,
+    BsDropdownModule.forRoot()],
   providers: [AuthService,
     ErrorInterceptorProvider,
     AlertifyService],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- DatingSiteSPA/src/app/nav/nav.component.ts
+++ DatingSiteSPA/src/app/nav/nav.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/Alertify.service';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
 export class NavComponent  {
+  isCollapsed = false;
 
   constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
