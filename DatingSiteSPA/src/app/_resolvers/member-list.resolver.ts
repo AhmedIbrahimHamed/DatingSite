777ef5808c62b7d520834c8d1b9b3cb6fac9ab26
AhmedIbrahimHamed <aih1175@gmail.com>
2020-09-11 00:26:42 +0200

    Fixed responsivness of member-list

--- DatingSiteSPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingSiteSPA/src/app/_resolvers/member-list.resolver.ts
@@ -14,7 +14,7 @@ import { catchError } from 'rxjs/operators';
 })
 export class MemberListResolver implements Resolve<User[]> {
   pageNumber = '1';
-  pageSize = '5';
+  pageSize = '6';
 
   constructor(private userService: UserService, private router: Router,
               private alertify: AlertifyService) { }
