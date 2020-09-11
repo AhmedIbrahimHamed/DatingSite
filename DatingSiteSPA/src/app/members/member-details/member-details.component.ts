e21de20e21c94f58db62ab07d153e5674e3a3a4e
AhmedIbrahimHamed <aih1175@gmail.com>
2020-09-11 19:20:07 +0200

    Added functionllity to envlope buttons to go to messages for user

--- DatingSiteSPA/src/app/members/member-details/member-details.component.ts
+++ DatingSiteSPA/src/app/members/member-details/member-details.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnChanges, HostListener } from '@angular/core';
+import { Component, OnInit, OnChanges, HostListener, ViewChild, AfterViewInit } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/Alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 
 import { User } from 'src/app/_models/user';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-details',
@@ -12,6 +13,8 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./member-details.component.css'],
 })
 export class MemberDetailsComponent implements OnInit {
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
+
   user: User;
 
   galleryOptions: NgxGalleryOptions[];
@@ -26,6 +29,11 @@ export class MemberDetailsComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe((data) => {
       this.user = data.users;
+    });
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params.tab;
+      this.selectTab(selectedTab);
     });
 
     this.galleryOptions = [
@@ -62,6 +70,10 @@ export class MemberDetailsComponent implements OnInit {
     }
 
     return imageUrls;
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 
 }
