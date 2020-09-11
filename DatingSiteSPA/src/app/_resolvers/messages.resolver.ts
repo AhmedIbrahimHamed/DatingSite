7f6806a00d98c5c08fd697b560ebdce1b85bd849
AhmedIbrahimHamed <aih1175@gmail.com>
2020-09-11 18:07:58 +0200

    Added messages component template

--- DatingSiteSPA/src/app/_resolvers/messages.resolver.ts
+++ DatingSiteSPA/src/app/_resolvers/messages.resolver.ts
@@ -15,7 +15,7 @@ import { AuthService } from '../_services/auth.service';
 })
 export class MessagesResolver implements Resolve<Message[]> {
   pageNumber = '1';
-  pageSize = '6';
+  pageSize = '5';
   messageContainer = 'Unread';
 
   constructor(private userService: UserService, private router: Router,
